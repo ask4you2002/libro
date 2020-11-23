b0306f69c6024c4d26b508f4ce0cc2530d7c7a94
Jaime Alvarez <jarabe@Jarabe.jarabe>
2020-11-23 09:13:06 +0100

    1874.05 numeroreferencia

--- Libro.java
+++ Libro.java
@@ -14,6 +14,7 @@ public class Libro {
     private String titulo;
     private int numeroPaginas;
     private String mensaje;
+    private String numeroReferencia;
 
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
@@ -23,6 +24,10 @@ public class Libro {
         autor = autorLibro;
         titulo = tituloLibro;
         numeroPaginas = paginas;
+        numeroReferencia = "";
+        if (numeroReferencia == ""){
+            numeroReferencia = "ZZZ";
+        }
     }
 
     public String getAutorLibro(){
@@ -36,19 +41,32 @@ public class Libro {
     public int getPaginas(){
         return numeroPaginas;
     }
-
+    
     public String  getDetalles(){
-        return mensaje = ("Titulo: " + titulo + ", Autor: " + autor + ", Paginas: " + numeroPaginas + "");
+        return mensaje = "Titulo: " + titulo + ", Autor: " + autor + ", Paginas: " + numeroPaginas + ", Numero referencia: " + numeroReferencia + "";
     }
-
+        
+    public String getNumeroReferencia(){
+        return numeroReferencia;
+    }   
+        
+    public void setNumeroReferencia(String numeroReferencia){
+        if (numeroReferencia.length() < 3){
+            System.out.println("Error: numero de referencia como minimo 3 caracteres.");
+        }
+        else {
+            this.numeroReferencia = numeroReferencia;
+        }
+    }
+        
     public void  imprimirDetalles(){ 
-        System.out.println("Titulo: " + titulo + ", Autor: " + autor + ", Paginas: " + numeroPaginas + "");
+        System.out.println("Titulo: " + titulo + ", Autor: " + autor + ", Paginas: " + numeroPaginas + ", Numero referencia: " + numeroReferencia + "");
     }
-
+    
     public void  imprimeAutor() {
         System.out.println(""+ autor +"");
     }
-
+    
     public void  imprimeTitulo() {
         System.out.println(""+ titulo +"");
     }
